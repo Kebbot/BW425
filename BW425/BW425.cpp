a01d5a5c0cdc09fe30830db31ecdbc87b7cf71dd
Nikolz <126600682+Kebbot@users.noreply.github.com>
2024-11-26 18:41:06 +0300

    Comit#1
    New project

---
+++ BW425/BW425.cpp
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <fstream>
+#include <cmath>
+#include <io.h>
+#include "Header.h"
+using namespace std;
+
+struct Human
+{
+    int* countHead;
+};
+
+void setHead(int*& name, int size)
+{
+    name = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        name[i] = 1;
+    }
+}
+
+void setHead(Human& name, int size)
+{
+    name.countHead = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        name.countHead[i] = 1;
+    }
+}
+
+
+int main()
+{
+    system("chcp 1251 > nul");
+    //setlocale(LC_ALL, "Rus");
+    //SetConsoleOutputCP(1251);
+    //SetConsoleCP(1251);
+    srand(time(NULL));
+
+    Human humanH;
+    int size = 5;
+    //setHead(humanH.countHead, size);
+    setHead(humanH, size);
+    for (int i = 0; i < size; i++)
+    {
+       cout << humanH.countHead[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
---
+++ BW425/Source.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <fstream>
+#include <cmath>
+#include <io.h>
+#include "Header.h"
+using namespace std;
+
+void Print()
+{
+	cout << "Привет Мир!" << endl;
+}
